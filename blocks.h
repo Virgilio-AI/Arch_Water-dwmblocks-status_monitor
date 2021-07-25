ca7e0153ddc779c2d10d9d834fb861a075424014
Virgilio <virgiliomurilloochoa1@gmail.com>
2021-07-25 15:46:02 -0500

    reordered blocks and added color

--- blocks.h
+++ blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"^c#47FAE9^",	"sb-music ^c#FFFFFF^#",	1,	11},
-	{"^c#F4FF00^^c#FFFFFF^",	"sb-price btc Bitcoin",				1,	21},
-	{"",	"sb-volume",	0,	10},
-	{"",	"sb-battery",	5,	3},
-	{"",	"sb-clock",	60,	1},
-	{"",	"sb-internet",	5,	4},
-
+	{"^c#B400FF^",	"sb-music ",	1,	11},
+	{"^c#F4FF00^^c#FFFFFF^",	"sb-price btc Bitcoin ",				1,	21},
+	{"^c#43BAD6^^c#FFFFFF^",	"sb-price ada Cardano ",				1,	21},
+	{"^c#B400FF^",	"sb-volume",	0,	10},
+	{"^c#FFFFFF^",	"sb-clock",	60,	1},
+	{"^c#00F3FF^",	"sb-internet",	5,	4},
+	{"","sb-battery",	5,	3},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
