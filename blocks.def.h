434b06709b870f0c6b80f268656eca144a140ef2
Virgilio <virgiliomurilloochoa1@gmail.com>
2021-07-25 20:10:54 -0500

    added icon to the date and time

--- blocks.def.h
+++ blocks.def.h
@@ -5,11 +5,11 @@ static const Block blocks[] = {
 	{"^c#F4FF00^^c#FFFFFF^",	"sb-price btc Bitcoin ",				1,	21},
 	{"^c#43BAD6^^c#FFFFFF^",	"sb-price ada Cardano ",				1,	21},
 	{"^c#B400FF^",	"sb-volume",	0,	10},
-	{"^c#FFFFFF^",	"sb-clock",	60,	1},
+	{"^c#FFFFFF^ ",	"sb-clock",	60,	1},
 	{"^c#00F3FF^",	"sb-internet",	5,	4},
 	{"","sb-battery",	5,	3},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "| ";
 static unsigned int delimLen = 5;
